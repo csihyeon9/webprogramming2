7db3c380d283d38e7c0801b1c9c41717012ac9ca
csihyeon9 <110538039+csihyeon9@users.noreply.github.com>
2022-11-25 14:12:40 +0900

    Add files via upload

--- js로 캔버스 제작/build_game.js
+++ js로 캔버스 제작/build_game.js
@@ -23,18 +23,35 @@ let brickHeight = 20;
 let brickPadding = 15;
 let brickOffsetTop = 30;
 let brickOffsetLeft = 35;
-let bricks = []; 
+let bricks = [];
+
+let score = 0;
+let lives = 3;
 
 for(let c=0; c<brickColumnCount; c++){ //벽동배열
     bricks[c] = [];
     for(let r=0; r<brickRowCount; r++){ //좌표 담음
-        bricks[c][r] = {x:0, y:0};
+        bricks[c][r] = {x:0, y:0, status:1};
     }
 }
 
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
+function mouseMoveHandler(e){
+    let relativeX= e.clientX - canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.width){
+        paddleX = relativeX - paddleWidth/2;
+    }
+}
+
+function drawLives() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Lives: "+lives, canvas.width-65, 20);
+}
 
 function keyDownHandler(e){
     if(e.KeyCode == "Right" || e.key == "ArrowRight"){
@@ -52,6 +69,31 @@ function keyUpHandler(e){
     else if(e.KeyCode == "left" || e.key == "ArrowLeft"){
         leftPressed = false;
     }
+}
+
+function collisionDetection(){
+    for(let c=0; c<brickColumnCount; c++){
+        for(let r=0; r<brickRowCount; r++){
+            let b = bricks[c][r];
+            if(b.status == 1){
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight){
+                    dy = -dy;
+                    b.status = 0;
+                    score++;
+                    if(score == brickColumnCount*brickColumnCount){
+                        alert("어케 했누");
+                        document.location.reload();
+                    }
+                }
+            }
+        }
+    }
+}
+
+function drawScore(){
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: "+score, 8, 20);
 }
 
 function drawBall(){
@@ -73,17 +115,18 @@ function drawPaddle(){
 function drawBricks(){
     for(let c=0; c<brickColumnCount; c++){
         for( let r=0; r<brickRowCount; r++){
-            let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-            let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#A6EAF1";
-            ctx.fill();
-            ctx.closePath();
+            if(bricks[c][r].status==1){
+                let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+                let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#A6EAF1";
+                ctx.fill();
+                ctx.closePath();
 
-
+            }
         }
     }
 }
@@ -93,6 +136,9 @@ function draw(){
     drawBricks();
     drawBall(); //ball
     drawPaddle();
+    drawScore();
+    collisionDetection();
+    drawLives();
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius){     /*벽 튕기기*/
         dx = -dx;
     }
@@ -116,9 +162,17 @@ function draw(){
         if(x > paddleX && x < paddleX + paddleWidth){
             dy = -dy;
         }
-        else{
+        if(!lives){
             alert("Game Over");
             document.location.reload();
+            clearInterval(interval);
+        }
+        else {
+            x = canvas.width/2;
+            y = canvas.height-30;
+            dx = 2;
+            dy = -2;
+            paddleX = (canvas.width-paddleWidth)/2;
         }
     }
 }
@@ -126,4 +180,4 @@ function draw(){
 
 
 
-setInterval(draw, 10);
+let interval = setInterval(draw, 10);
